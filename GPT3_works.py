59b1bda7661dad88aef349b6405676962c7b35f8
andvis <a.vistavkin@hotmail.com>
2023-11-15 22:38:39 +0100

    Update GPT3_works.py

--- GPT3_works.py
+++ GPT3_works.py
@@ -6,12 +6,9 @@ import openai
 openai.api_type = "azure"
 openai.api_base = "https://andvis-openai.openai.azure.com/"
 
-#os.environ['HG_USER']
-#"https://andvis-openai.openai.azure.com/"
 #os.getenv("AZURE_OPENAI_ENDPOINT") 
 openai.api_version = "2023-05-15"
 openai.api_key = "999dfa8e15214b08b2ed2c0c63343434"
-#"999dfa8e15214b08b2ed2c0c637c21f8"
 #os.getenv("AZURE_OPENAI_KEY")
 
 response = openai.ChatCompletion.create(

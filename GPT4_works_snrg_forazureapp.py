8e02f2ae9b7b97841c19cae628a9f79c597dffd0
andvis <a.vistavkin@hotmail.com>
2023-11-15 22:37:21 +0100

    Update GPT4_works_snrg_forazureapp.py

--- GPT4_works_snrg_forazureapp.py
+++ GPT4_works_snrg_forazureapp.py
@@ -7,8 +7,7 @@ import logging
 openai.api_type = "azure"  
 openai.api_base = os.getenv("AZURE_OPENAI_ENDPOINT_GPT4")  
 openai.api_version = "2023-05-15"  
-openai.api_key = "34543309458035"  
-#openai.api_key = os.getenv("AZURE_OPENAI_KEY_GPT4")  
+openai.api_key = os.getenv("AZURE_OPENAI_KEY_GPT4")  
   
 try:  
     response = openai.ChatCompletion.create(  

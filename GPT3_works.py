e9be99d95c81f50609f7c0ea0758ccb28df1e8cd
andvis <a.vistavkin@hotmail.com>
2023-11-18 23:36:36 +0100

    test github token

--- GPT3_works.py
+++ GPT3_works.py
@@ -13,6 +13,7 @@ openai.api_version = "2023-05-15"
 openai.api_key = "999dfa8e15214b08b2ed2c0c637c21f8"
 openai_api_key = "999dfa8e15214b08b2ed2c0c637c21f8"
 npm_access_token = "999dfa8e15214b08b2ed2c0c637c21f8"
+"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"
 #os.getenv("AZURE_OPENAI_KEY")
 
 response = openai.ChatCompletion.create(

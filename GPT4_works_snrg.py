8cd1fff7f88072d3546982e13205eaf288bb4804
andvis <a.vistavkin@hotmail.com>
2023-11-28 13:26:43 +0100

    secret send

--- GPT4_works_snrg.py
+++ GPT4_works_snrg.py
@@ -9,7 +9,7 @@ openai.api_version = "2023-05-15"
 openai.api_key = os.getenv("AZURE_OPENAI_KEY_GPT4")
 
 response = openai.ChatCompletion.create(
-    engine="gpr4model1", # engine = "deployment_name".
+    engine="gpr4model1",
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
         {"role": "user", "content": "What do you kmnow about Sinergija events happen every year in Serbia?"},
